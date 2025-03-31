228febd2783204144717b5d9e124f11007d30676
Juan David Mendivelso Zuleta <juanmendivelso699@gmail.com>
2025-03-31 18:22:15 -0500

    Actualización index

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 // Your 7 tasks are located in separate files.
 // Open task1.js to begin.
 
-import {costCalculator} from "./task1.js";
+import * as task1 from "./task1.js";
 import * as task2 from "./task2.js";
-import {ageCalculator} from "./task3.js";
-console.log(ageCalculator(2000, 12, 25));
+import * as task3 from "./task3.js";
 import * as task4 from "./task4.js";
 import * as task5 from "./task5.js";
 import * as task6 from "./task6.js";
